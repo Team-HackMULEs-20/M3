d9e7add1183c074bbe19109ba1a2d4187eb6b7d6
jhoffman1 <jhoffman40@gatech.edu>
2015-09-13 20:59:17 -0400

    player class

--- Game_Config/src/sample/Player.java
+++ Game_Config/src/sample/Player.java
@@ -2,6 +2,29 @@ package sample;
 
 public class Player {
 
+    int number;
+    String name;
+    int race;
+    String color;
+
+    public Player (int number, String name, int race, String color) {
+        this.number = number;
+        this.name = name;
+        //this.race = race;
+        this.color = color;
+    }
+
+    public enum Race {
+        FLAPPER, BONZOID, UGAITE, BUZZITE, HUMAN
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor(){
+        return color;
+    }
 
 
 }
