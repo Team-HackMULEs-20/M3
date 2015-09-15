4dfd2498d9a6938120722315221216f8ca2b80c6
jhoffman1 <jhoffman40@gatech.edu>
2015-09-15 16:13:06 -0400

    working on Player and Timer Classes

--- gameConfig/Player.java
+++ gameConfig/Player.java
@@ -2,20 +2,32 @@ package gameConfig;
 
 public class Player {
 
-    int number;
-    String name;
-    int race;
-    String color;
+    private int number;
+    private String name;
+    private int race;
+    private String color;
+    private int location; //figure out
+
+    private int money;
+    private int food;
+    private int energy;
+    private int ore;
 
     public Player (int number, String name, int race, String color) {
         this.number = number;
         this.name = name;
-        //this.race = race;
+        this.race = race; //how to import?
         this.color = color;
+
+//say beginner/standard/tournament
     }
 
     public enum Race {
         FLAPPER, BONZOID, UGAITE, BUZZITE, HUMAN
+    }
+
+    public enum Location {
+
     }
 
     public String getName() {
--- gameConfig/Timer.java
+++ gameConfig/Timer.java
@@ -1,10 +1,10 @@
 package gameConfig;
 //package fxtimer;
 
-        import javafx.application.Application;
-        import javafx.event.ActionEvent;
-        import javafx.event.EventHandler;
-        import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
 
 public class Timer extends Application{
     int beginTime;
