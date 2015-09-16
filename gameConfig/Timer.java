41a12a60f2abfef46a14bdf040ddcc1b58e68f3c
jhoffman1 <jhoffman40@gatech.edu>
2015-09-16 15:53:29 -0400

    Timer displays number

--- gameConfig/Timer.java
+++ gameConfig/Timer.java
@@ -4,26 +4,51 @@ package gameConfig;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.layout.VBox;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.animation.Timeline;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.scene.layout.VBox;
 
 public class Timer extends Application{
-    private int beginTime;
+    private int beginTime = 50; //remove 50 and uncomment constructor
+    private Integer timeLeft = beginTime; //comment out
     private Timeline timeline;
     private Label timerLabel = new Label();
 
-    public Timer(int beginTime) {
-        this.beginTime = beginTime;
+    //public Timer(int beginTime) {
+    //    this.beginTime = beginTime;
+    //      timeLeft = beginTime;
+    //}
+
+    public static void main(String[] args) {
+        Application.launch(args);
     }
 
     @Override
     public void start (Stage stage) {
+        stage.setTitle("Turn Timer");
+        Group t = new Group();
+        Scene scene = new Scene(t,150,150);
 
-    }
+        timerLabel.setText(timeLeft.toString());
+        timerLabel.setTextFill(Color.BLACK);
+        timerLabel.setStyle("-fx-font-size: 5em;");
 
-    public static void main(String[] args) {
-        Application.launch(args);
+        VBox box = new VBox();
+        box.setAlignment(Pos.CENTER);
+        box.setPrefWidth(scene.getWidth());
+        box.setLayoutY(30);
+        box.getChildren().add(timerLabel);
+        t.getChildren().add(box);
+
+        stage.setScene(scene);
+        stage.show();
+
     }
 
 }
