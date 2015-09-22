e35c0849f7e7d3ea349cdcc1834a60d1ab891f62
kfindley7 <kfindleygt@gmail.com>
2015-09-22 12:58:01 -0400

    Update Turns.java

--- gameConfig/Turns.java
+++ gameConfig/Turns.java
@@ -1,16 +1,44 @@
 package gameConfig;
 
 public class Turns {
-    Player[] players;
+    public static Player[] players;
     public static int rounds;
+    public static int timeOfTurn;
 
     public Turns(Player[] players) {
         this.players = players;
-        rounds = 0;
+        rounds = 1;
     }
 
-//    public int getRounds() {
-//        return rounds;
-//    }
+    public static Player getNextTurn() {
+        return players[0];
+    }
 
+    public static int timeForTurn(Player player) {
+        int foodCount = player.getFood();
+        if (foodCount == 0) {
+            timeOfTurn = 5;
+        } else {
+            if (rounds >= 1 && rounds <= 4) {
+                if (foodCount < 3) {
+                    timeOfTurn = 30;
+                } else {
+                    timeOfTurn = 50;
+                }
+            } else if (rounds >= 5 && rounds <= 8) {
+                if (foodCount < 4) {
+                    timeOfTurn = 30;
+                } else {
+                    timeOfTurn = 50;
+                }
+            } else if (rounds >= 9 && rounds <= 12) {
+                if (foodCount < 5) {
+                    timeOfTurn = 30;
+                } else {
+                    timeOfTurn = 50;
+                }
+            }
+        }
+        return timeOfTurn;
+    }
 }
