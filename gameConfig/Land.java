223d754fd0fb503d5a29c2bc18ad8af78b55b88d
Christopher Ford <christopherford123@gmail.com>
2015-10-02 13:40:19 -0400

    All changes to land class. Changed constructor and added setOwner.

--- gameConfig/Land.java
+++ gameConfig/Land.java
@@ -1,54 +1,62 @@
 package gameConfig;
 
 public class Land {
-    private Player owner;
-    private boolean owned;
-    private int row;
-    private int col;
+	private Player owner;
+	private boolean owned;
+	private int row;
+	private int col;
 
-    public static boolean landBuyEnable = false;
-    
-    // private hasMule //TODO
-    // private numMule //If a mule is on the property when sold, it is lost
-    //randomNum = minimum + (int)(Math.random()*maximum);
+	public static boolean landBuyEnable = false;
 
-    public Land(int row, int col) {
-        this.row = row;
-        this.col = col;
-        owned = false;
-    }
+	private boolean hasMule;
 
-    public int getRow() {return row;}
-    public int getCol() {return col;}
 
-    public Player getOwner() {return owner;}
+	// private hasMule //TODO
+	// private numMule //If a mule is on the property when sold, it is lost
+	//randomNum = minimum + (int)(Math.random()*maximum);
 
-    public static int getBuyPrice() {
-        return 300 + Turns.rounds + (int)(Math.random() * 100);
-    }
-    public int getSellPrice() {
-        return 400 + (int)(Math.random() * 200);
-    }
+	public Land(int col, int row) {
+		this.row = row;
+		this.col = col;
+		owned = false;
+		hasMule = false;
+	}
 
-    public void buyLand(Player p) {
-        if (!isOwned()) {
-            owner = p;
-            owned = true;
-            owner.addSubMoney(getBuyPrice());
-        } else {
-            System.out.println("This land is already owned"); //TODO
-        }
-    }
+	public int getRow() {return row;}
+	public int getCol() {return col;}
 
-    public void sellLand() {
-        if (isOwned()) {
-            owner.addSubMoney(getSellPrice());
-            owner = null;
-            owned = false;
-        } else {
-            System.out.println("This land is not owned to sell"); //TODO
-        }
-    }
+	public Player getOwner() {return owner;}
 
-    public boolean isOwned() {return owned;}
+	public static int getBuyPrice() {
+		return 300 + Turns.rounds + (int)(Math.random() * 100);
+	}
+	public int getSellPrice() {
+		return 400 + (int)(Math.random() * 200);
+	}
+
+	public void setOwner(Player p) {
+		owner = p;
+		owned = true;
+	}
+
+	public void buyLand(Player p) {
+		if (!isOwned()) {
+			setOwner(p);
+			owner.addSubMoney(getBuyPrice());
+		} else {
+			System.out.println("This land is already owned"); //TODO
+		}
+	}
+
+	public void sellLand() {
+		if (isOwned()) {
+			owner.addSubMoney(getSellPrice());
+			owner = null;
+			owned = false;
+		} else {
+			System.out.println("This land is not owned to sell"); //TODO
+		}
+	}
+
+	public boolean isOwned() {return owned;}
 }
