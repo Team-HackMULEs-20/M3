87987d6d378e108230ed96a22fc6892d166d00a6
cag2011 <cag2011@gmail.com>
2015-11-18 10:49:10 -0500

    Saved Message Box

--- gameConfig/LoadSaveGame.java
+++ gameConfig/LoadSaveGame.java
@@ -1,5 +1,15 @@
 package gameConfig;
 
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -55,5 +65,31 @@ public class LoadSaveGame {
             e.printStackTrace();
         }
         return new ArrayList<>();
+    }
+    
+    public static void messageBox() {
+        Stage savedStage = new Stage();
+        savedStage.setTitle("Saved");
+        savedStage.setAlwaysOnTop(true);
+        Group t = new Group();
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setVgap(10);
+        grid.setHgap(10);
+        Label errorLabel = new Label("Your game has been successfully saved");
+        errorLabel.setFont(new Font("American Typewriter", 15));
+        Button errorButton = new Button("Ok");
+        errorButton.setFont(new Font("American Typewriter", 17));
+        GridPane grid2 = new GridPane();
+        grid2.setAlignment(Pos.CENTER);
+        grid2.add(errorButton, 0, 0);
+        grid.add(errorLabel, 1, 0);
+        grid.add(grid2, 1, 1);
+        t.getChildren().add(grid);
+        Scene errorScene = new Scene(t);
+        savedStage.setScene(errorScene);
+        savedStage.show();
+        savedStage.toFront();
+        errorButton.setOnAction((ActionEvent e) -> savedStage.close());
     }
 }
