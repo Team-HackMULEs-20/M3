e1cccf7c7598601e0680e8c0f7ad25099fd3fd88
Christopher Ford <christopherford123@gmail.com>
2015-11-29 17:47:19 -0500

    Land array creation now assigns crystite values.

--- gameConfig/LandType.java
+++ gameConfig/LandType.java
@@ -5,9 +5,9 @@ public enum LandType {
 
     public static LandType[] standardMap() {
         return new LandType[]{PLAIN, PLAIN, M1,    PLAIN, RIVER, PLAIN, M3,    PLAIN, PLAIN,
-                				PLAIN, M1,    PLAIN, PLAIN, RIVER, PLAIN, PLAIN, PLAIN, M3,
-                				M3,    PLAIN, PLAIN, PLAIN, TOWN,  PLAIN, PLAIN, PLAIN, M1,
-                				PLAIN, M2,    PLAIN, PLAIN, RIVER, PLAIN, M2,    PLAIN, PLAIN,
-                				PLAIN, PLAIN, M2,    PLAIN, RIVER, PLAIN, PLAIN, PLAIN, M2};
+                			  PLAIN, M1,    PLAIN, PLAIN, RIVER, PLAIN, PLAIN, PLAIN, M3,
+                			  M3,    PLAIN, PLAIN, PLAIN, TOWN,  PLAIN, PLAIN, PLAIN, M1,
+                			  PLAIN, M2,    PLAIN, PLAIN, RIVER, PLAIN, M2,    PLAIN, PLAIN,
+                			  PLAIN, PLAIN, M2,    PLAIN, RIVER, PLAIN, PLAIN, PLAIN, M2};
     }
 }
